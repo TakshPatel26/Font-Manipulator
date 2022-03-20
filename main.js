57d7e50338bfde16000fbc9d86a0169bfed45ac7
Taksh Patel <88882602+TakshPatel26@users.noreply.github.com>
2022-03-20 14:39:46 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ difference = 0;
 function setup(){
     video = createCapture(VIDEO);
     video.size(400,400);
-    video.position(10,50);
+    video.position(10,130);
     canvas = createCanvas(800,400);
     canvas.position(430,130);
     poseNet = ml5.poseNet(video,modelDone);
